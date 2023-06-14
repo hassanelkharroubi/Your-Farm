e366abde2a1609ce7bf4fd322ceb8bf704cb0bf6
Hassan EL KHARROUBI <hassanelkharroubi1997@gmail.com>
2023-06-14 21:12:37 +0100

    add new method for sending data

--- app/src/main/java/com/example/myfarm/FormActivity.java
+++ app/src/main/java/com/example/myfarm/FormActivity.java
@@ -6,6 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.IOException;
+
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class FormActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText mNitrigenText;
@@ -15,6 +23,7 @@ public class FormActivity extends AppCompatActivity implements View.OnClickListe
     private EditText mRainText;
     private EditText mTempText;
     private EditText mHumText;
+    private static final String END_POST_API="http://192.168.1.51:5000/soule";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +40,42 @@ public class FormActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
-
-
     }
+
+    public static void sendFormData() {
+        OkHttpClient client = new OkHttpClient();
+
+        // Build the request body
+        RequestBody formBody = new FormBody.Builder()
+                .add("param1", "value1")
+                .add("param2", "value2")
+                .add("param3", "value3")
+                .add("param4", "value4")
+                .add("param5", "value5")
+                .add("param6", "value6")
+                .add("param7", "value7")
+                .build();
+
+        // Build the request
+        Request request = new Request.Builder()
+                .url("http://example.com/submit")
+                .post(formBody)
+                .build();
+
+        // Send the request
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                System.out.println("Form data sent successfully");
+                // Process the response here
+            } else {
+                System.out.println("Form data sending failed");
+                // Handle unsuccessful response
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle network or IO error
+        }
+    }
+
 }
