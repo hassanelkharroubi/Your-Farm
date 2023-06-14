ad76e67593232534ac84b535f035b2a8fd8ffdc9
Hassan EL KHARROUBI <hassanelkharroubi1997@gmail.com>
2023-06-14 21:05:37 +0100

    add new input text

--- app/src/main/java/com/example/myfarm/FormActivity.java
+++ app/src/main/java/com/example/myfarm/FormActivity.java
@@ -3,12 +3,36 @@ package com.example.myfarm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
-public class FormActivity extends AppCompatActivity {
+public class FormActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mNitrigenText;
+    private EditText mPhosohoText;
+    private EditText mPotText;
+    private EditText mPHText;
+    private EditText mRainText;
+    private EditText mTempText;
+    private EditText mHumText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
+        mNitrigenText=findViewById(R.id.Nitrogen);
+        mPhosohoText=findViewById(R.id.Phosphorous);
+        mPotText=findViewById(R.id.Potassium);
+        mPotText=findViewById(R.id.pH_level);
+        mPHText=findViewById(R.id.Rainful);
+        mTempText=findViewById(R.id.temperature);
+        mHumText=findViewById(R.id.humidity);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+
     }
 }
