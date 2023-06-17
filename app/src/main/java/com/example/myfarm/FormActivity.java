51faa95597aecdaa0c1ceaf1cb2c9880129ead83
Hassan EL KHARROUBI <hassanelkharroubi1997@gmail.com>
2023-06-17 12:50:48 +0100

    remove lib of rounded image

--- app/src/main/java/com/example/myfarm/FormActivity.java
+++ app/src/main/java/com/example/myfarm/FormActivity.java
@@ -15,9 +15,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
-import com.bumptech.glide.request.RequestOptions;
 import com.example.myfarm.config.Network;
 
 import org.json.JSONException;
