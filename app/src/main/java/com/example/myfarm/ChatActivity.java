ff959cd8d0a600479787e5d4d33ddbd42bf1a615
hassanelkharroubi <hassanelkharroubi1997@gmail.com>
2023-06-17 16:09:43 +0100

    add progress bar

--- app/src/main/java/com/example/myfarm/ChatActivity.java
+++ app/src/main/java/com/example/myfarm/ChatActivity.java
@@ -1,11 +1,13 @@
 package com.example.myfarm;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -31,11 +33,18 @@ public class ChatActivity extends AppCompatActivity {
 
     private ChatAdapter chatAdapter;
     private List<Message> dataList ;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        // Initialize the progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Please wait...");
+        progressDialog.setCancelable(false);
+
         dataList= new ArrayList<>();
         recyclerView = findViewById(R.id.recyclerView);
         inputEditText = findViewById(R.id.inputEditText);
@@ -63,6 +72,7 @@ public class ChatActivity extends AppCompatActivity {
             inputEditText.setText("");
             // Send the message to the API and handle the response
             // You'll implement this in later steps
+            progressDialog.show();
             sendToAPI(message);
         }
     }
@@ -90,6 +100,8 @@ public class ChatActivity extends AppCompatActivity {
                 // Handle the failure
                 e.printStackTrace();
                 Log.e(TAG,e.getMessage());
+                Toast.makeText(ChatActivity.this, "Please retry... timeout error", Toast.LENGTH_SHORT).show();
+                progressDialog.dismiss();
             }
 
             @Override
@@ -108,12 +120,14 @@ public class ChatActivity extends AppCompatActivity {
                             dataList.add(chatMessage);
                             chatAdapter.addMessage(chatMessage);
                             chatAdapter.notifyDataSetChanged();
+                            progressDialog.dismiss();
                         }
                     });
                 } else {
                     // Handle unsuccessful response
                     String errorMessage = response.message();
                     Log.e(TAG,"Error mesage "+errorMessage);
+                    progressDialog.dismiss();
                     // Handle the error message
                 }
             }
